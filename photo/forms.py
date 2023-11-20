137c9882b0b08721b7668c048735dab725cd092c
r-sugawara-1987 <spr2340854@stu.o-hara.ac.jp>
2023-11-20 10:24:16 +0900

    Commit

---
+++ photo/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm
+from .models import PhotoPost
+
+class PhotoPostForm(ModelForm):
+    '''
+    ModelFormのインナークラス
+    '''
+    class Meta:
+        '''
+        ModelFormのインナークラス
+
+        Attributes:
+            model: モデルのクラス
+            fields: フォームで使用するモデルのフィールドを指定
+        '''
+        model = PhotoPost
+        fields = ['category', 'title', 'comment', 'image1', 'image2']
